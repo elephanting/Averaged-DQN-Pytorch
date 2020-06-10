d6c81ac52a94454568e78ad0130145e394e19ad2
elephanting <elephanting5566@gmail.com>
2020-06-10 12:29:53 +0800

    init commit

---
+++ DQN.py
@@ -0,0 +1,197 @@
+"""
+    This file is copied/apdated from https://github.com/higgsfield/RL-Adventure/blob/master/1.dqn.ipynb
+"""
+
+import math, random
+
+import gym
+import numpy as np
+import argparse
+import copy
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.autograd as autograd 
+import torch.nn.functional as F
+
+import matplotlib.pyplot as plt
+
+# use CUDA
+USE_CUDA = torch.cuda.is_available()
+Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
+
+# Atari Environment
+from common.wrappers import make_atari, wrap_deepmind, wrap_pytorch
+
+env_id = "Breakout-v0"
+env    = make_atari(env_id)
+env    = wrap_deepmind(env)
+env    = wrap_pytorch(env)
+
+
+# replay buffer
+from collections import deque
+
+class ReplayBuffer(object):
+    def __init__(self, capacity):
+        self.buffer = deque(maxlen=capacity)
+    
+    def push(self, state, action, reward, next_state, done):
+        state      = np.expand_dims(state, 0)
+        next_state = np.expand_dims(next_state, 0)
+            
+        self.buffer.append((state, action, reward, next_state, done))
+    
+    def sample(self, batch_size):
+        state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
+        return np.concatenate(state), action, reward, np.concatenate(next_state), done
+    
+    def __len__(self):
+        return len(self.buffer)
+
+replay_initial = 10000
+replay_buffer = ReplayBuffer(100000)
+
+
+# Deep Q Network
+class DQN(nn.Module):
+    def __init__(self, input_shape, num_actions):
+        super(DQN, self).__init__()
+        
+        self.input_shape = input_shape
+        self.num_actions = num_actions
+        
+        self.features = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+        
+        self.fc = nn.Sequential(
+            nn.Linear(self.feature_size(), 512),
+            nn.ReLU(),
+            nn.Linear(512, self.num_actions)
+        )
+        
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+    
+    def feature_size(self):
+        return self.features(autograd.Variable(torch.zeros(1, *self.input_shape))).view(1, -1).size(1)
+    
+    def act(self, state, epsilon):
+        if random.random() > epsilon:
+            state   = Variable(torch.FloatTensor(np.float32(state)).unsqueeze(0), volatile=True)
+            q_value = self.forward(state)
+            action  = q_value.max(1)[1].data[0]
+        else:
+            action = random.randrange(env.action_space.n)
+        return action
+
+model = DQN(env.observation_space.shape, env.action_space.n)
+target_model = DQN(env.observation_space.shape, env.action_space.n)
+if USE_CUDA:
+    model = model.cuda()
+
+
+def compute_td_loss(batch_size):
+    state, action, reward, next_state, done = replay_buffer.sample(batch_size)
+
+    state      = Variable(torch.FloatTensor(np.float32(state)))
+    next_state = Variable(torch.FloatTensor(np.float32(next_state)), volatile=True)
+    action     = Variable(torch.LongTensor(action))
+    reward     = Variable(torch.FloatTensor(reward))
+    done       = Variable(torch.FloatTensor(done))
+
+    q_values      = model(state)
+    next_q_values = target_model(next_state)
+
+    q_value          = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
+    next_q_value     = next_q_values.max(1)[0]
+    expected_q_value = reward + gamma * next_q_value * (1 - done)
+    
+    loss = (q_value - Variable(expected_q_value.data)).pow(2).mean()
+        
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    return loss
+
+def plot(frame_idx, rewards, losses):
+    plt.figure(figsize=(20,5))
+    plt.subplot(131)
+    plt.title('frame %s. reward: %s' % (frame_idx, np.mean(rewards[-10:])))
+    plt.plot(rewards)
+    plt.subplot(132)
+    plt.title('loss')
+    plt.plot(losses)
+    #plt.show()
+       
+optimizer = optim.Adam(model.parameters(), lr=0.00001)
+
+# Epsilon greedy exploration
+epsilon_start = 1.0
+epsilon_final = 0.01
+epsilon_decay = 30000
+
+epsilon_by_frame = lambda frame_idx: epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * frame_idx / epsilon_decay)
+
+# training
+losses = []
+all_rewards = []
+frame_done = []
+episode_reward = 0
+
+num_frames = 1400000
+batch_size = 32
+gamma      = 0.99
+
+state = env.reset()
+episode = 0
+for frame_idx in range(1, num_frames + 1):
+    epsilon = epsilon_by_frame(frame_idx)
+    action = model.act(state, epsilon)
+    
+    next_state, reward, done, _ = env.step(action)
+    replay_buffer.push(state, action, reward, next_state, done)
+    
+    state = next_state
+    episode_reward += reward
+    
+    if done:
+        state = env.reset()
+        #env.render()
+        all_rewards.append(episode_reward)
+        frame_done.append(frame_idx)
+        episode_reward = 0
+        episode += 1
+        #if episode % 10 == 0:
+        #    print(episode)
+        
+    if len(replay_buffer) > replay_initial:
+        loss = compute_td_loss(batch_size)
+        #losses.append(loss.item())
+        
+    if frame_idx % 100000 == 0:
+        #plot(frame_idx, all_rewards, losses)
+        np.save('idx.npy', frame_idx)
+        np.save('reward.npy', all_rewards)
+        print(frame_idx)
+        print(all_rewards[-1])
+
+    if frame_idx % 100 == 0:
+        target_model = copy.deepcopy(model)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--average', action='store_true', help='perform Averaged-DQN')
+    parser.add_argument('--k', type=int, default=1, help='if perform Averaged-DQN, average k action values')
+    args = parser.parse_args()
---
+++ main.py
@@ -0,0 +1,90 @@
+"""
+    This file is copied/apdated from https://github.com/transedward/pytorch-dqn/blob/master/main.py
+"""
+
+import gym
+import torch.optim as optim
+import argparse
+
+from model import DQN
+from dqn_learn import OptimizerSpec, dqn_learing
+from utils.gym import get_env, get_wrapper_by_name
+from utils.schedule import LinearSchedule
+
+BATCH_SIZE = 32
+GAMMA = 0.99
+REPLAY_BUFFER_SIZE = 1000000
+LEARNING_STARTS = 50000
+LEARNING_FREQ = 4
+FRAME_HISTORY_LEN = 4
+TARGER_UPDATE_FREQ = 10000
+LEARNING_RATE = 0.00025
+ALPHA = 0.95
+EPS = 0.01
+
+def main(env, num_timesteps):
+
+    def stopping_criterion(env):
+        # notice that here t is the number of steps of the wrapped env,
+        # which is different from the number of steps in the underlying env
+        return get_wrapper_by_name(env, "Monitor").get_total_steps() >= num_timesteps
+
+    optimizer_spec = OptimizerSpec(
+        constructor=optim.RMSprop,
+        kwargs=dict(lr=LEARNING_RATE, alpha=ALPHA, eps=EPS),
+    )
+
+    exploration_schedule = LinearSchedule(1000000, 0.1)
+
+    if args.averaged:
+        dqn_learing(
+            env=env,
+            q_func=DQN,
+            optimizer_spec=optimizer_spec,
+            exploration=exploration_schedule,
+            stopping_criterion=stopping_criterion,
+            replay_buffer_size=REPLAY_BUFFER_SIZE,
+            batch_size=BATCH_SIZE,
+            gamma=GAMMA,
+            learning_starts=LEARNING_STARTS,
+            learning_freq=LEARNING_FREQ,
+            frame_history_len=FRAME_HISTORY_LEN,
+            target_update_freq=TARGER_UPDATE_FREQ,
+            averaged=True,
+            k=args.k
+        )    
+    else:   
+        dqn_learing(
+            env=env,
+            q_func=DQN,
+            optimizer_spec=optimizer_spec,
+            exploration=exploration_schedule,
+            stopping_criterion=stopping_criterion,
+            replay_buffer_size=REPLAY_BUFFER_SIZE,
+            batch_size=BATCH_SIZE,
+            gamma=GAMMA,
+            learning_starts=LEARNING_STARTS,
+            learning_freq=LEARNING_FREQ,
+            frame_history_len=FRAME_HISTORY_LEN,
+            target_update_freq=TARGER_UPDATE_FREQ,
+            averaged=False,
+            k=args.k
+        )
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--averaged', action='store_true', help='perform Averaged-DQN')
+    parser.add_argument('--k', type=int, default=1, help='average k outputs')
+    args = parser.parse_args()
+
+    # Get Atari games.
+    benchmark = gym.benchmark_spec('Atari40M')
+
+    # Change the index to select a different game.
+    task = benchmark.tasks[3]
+
+    # Run training
+    seed = 0 # Use a seed of zero (you may want to randomize the seed!)
+    env = get_env(task, seed)
+
+    main(env, task.max_timesteps)
